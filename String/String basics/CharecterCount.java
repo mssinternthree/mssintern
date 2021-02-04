7130a8a81db006a3d61690ce98724f37305ed8b2
saikrishnabendalam <37179213+saikrishnabendalam@users.noreply.github.com>
2021-02-04 22:08:48 +0530

    updated files

--- String/String basics/CharecterCount.java
+++ String/String basics/CharecterCount.java
@@ -5,15 +5,20 @@ public class CharecterCount {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("enter string");
 		String s1=scan.nextLine();
-		System.out.println("enter Char to check");
-		char c=scan.next().charAt(0);
-		int charcount=0;
-		for(int i=0;i<s1.length();i++) {
-			if(s1.charAt(i)==c) {
-				charcount+=1;
+		//System.out.println("enter Char to check");
+		char[] c=s1.toCharArray();
+		
+		for(int i=0;i<c.length;i++) {
+			int chaCount=0;
+			for(int j=0;j<c.length;j++) {
+				if(c[i]==c[j]) {
+					chaCount+=1;
+				}
+				
 			}
+			System.out.println(c[i]+":"+chaCount);
 		}
-		System.out.println("there are total  "+charcount+" "+c+"'s in string");
+		
 	}
 
 }
