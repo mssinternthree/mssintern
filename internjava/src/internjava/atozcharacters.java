04413e7d2cb5bccc118c2185386e014a1a265938
saikumar1914 <saikumar14551821>
2021-02-08 20:04:30 +0530

    commit

---
+++ internjava/src/internjava/atozcharacters.java
@@ -0,0 +1,13 @@
+package internjava;
+
+public class atozcharacters {
+
+	public static void main(String[] args) {
+
+        char c;
+
+        for(c = 'a'; c <= 'z'; c++)
+            System.out.print(c + " ");
+        System.out.println("njasdf");
+    }
+}
